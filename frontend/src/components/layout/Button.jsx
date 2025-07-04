2273bf367f42afdb050c76441f3c3aff5f83f577
HipnosM <hipnosmiller@gmail.com>
2025-07-04 00:49:56 -0300

    :recycle: style: ajuste no estilo do botão quando possui ícone

--- frontend/src/components/layout/Button.jsx
+++ frontend/src/components/layout/Button.jsx
@@ -1,11 +1,16 @@
 import styles from "./Button.module.css";
 
-export default function Button({ customClass, text, icon, type="", onClick }) {
+export default function Button({ customClass, text, icon, type = "", onClick }) {
     const classNames = `${styles.button} ${customClass ? customClass.split(" ").map(cls => styles[cls] || cls).join(" ") : ""}`;
 
     return (
         <div>
-            <button type={type} className={classNames} onClick={type === "button" ? onClick : () => {}}>
+            <button
+                type={type}
+                className={classNames}
+                style={{ gap: icon ? "1rem" : "0"}}
+                onClick={type === "button" ? onClick : () => { }}
+            >
                 {text}
                 <span className={styles.icon}>{icon}</span>
             </button>
