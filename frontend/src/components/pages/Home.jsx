ce7300ec95047086161d066d8a6ac5947e2d2318
HipnosM <hipnosmiller@gmail.com>
2025-07-20 01:01:21 -0300

    :sparkles: feat: adicionado o modal de criação de tarefas

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import styles from "./Home.module.css";
 import hero from "../../assets/hero.svg"
 import Button from "../layout/Button";
 import { FaPlusCircle } from "react-icons/fa";
+import ModalTask from "../layout/ModalTask";
 
 export default function Home() {
+    const [modalOpen, setModalOpen] = useState(false);
+
     return (
         <section className={styles.home_container}>
             <h1 className={styles.title}><span>Gerencie</span> suas <span>tarefas</span> de forma simples e rápida!</h1>
@@ -13,7 +17,13 @@ export default function Home() {
             <Button
                 text="Criar tarefa"
                 icon={<FaPlusCircle />}
+                onClick={() => setModalOpen(true)}
             />
+
+            {modalOpen && <ModalTask
+                modalOpen={modalOpen}
+                onClose={() => setModalOpen(false)}
+            />}
         </section>
     );
 };
