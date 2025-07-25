7a7673ab55f44c86d3e7ba47725d759dd52e6467
HipnosM <hipnosmiller@gmail.com>
2025-07-25 13:10:48 -0300

    :recycle: refactor: correção no caminho das rotas

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(cors()); // Permite requisições do frontend
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/tasks", authMiddleware, taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", authMiddleware, taskRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
