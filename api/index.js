23c2af893d2f06f4964a07dcc69375289d209cc2
IagoRdgs <iagoandre567@gmail.com>
2025-08-16 14:17:06 -0300

    :bug: fix: adicionado o export default app para serverless

--- api/index.js
+++ api/index.js
@@ -37,14 +37,11 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", authMiddleware, taskRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
-
 // manter database ativa a cada 5 dias
 setInterval(async () => {
-    console.log("Mantendo o banco de dados ativo...");
-    const users = await prisma.user.findMany();
-    console.log(`Usuários ativos: ${users.length}`);
+  console.log("Mantendo o banco de dados ativo...");
+  const users = await prisma.user.findMany();
+  console.log(`Usuários ativos: ${users.length}`);
 }, 432000000); // 5 dias em milissegundos
+
+export default app;
